f734e5eff7dee04e55b34af40f9d2be9474125ca
Benjamin <cunningham1030@yahoo.com>
2018-02-24 16:40:48 +0800

    4.3 Added landing_page as root that features one product, created an orders controller containing methods (index, show, new, create, and destroy), created views for orders#index and orders#show, updated routes.rb to include orders as a resource.

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,8 +1,16 @@
 class SimplePagesController < ApplicationController
+
   def index
   end
+
   def about
   end
+
   def contact
   end
+
+  def landing_page
+    @featured_product = Product.first
+  end
+  
 end
