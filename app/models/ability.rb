b59b51fade17c9b3fc12f93b18475caf75d4bdeb
Benjamin <cunningham1030@yahoo.com>
2018-04-10 23:23:05 +0800

    5.10b Added end to end of if-else statement in ability.rb model.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,9 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.admin?
       can :manage, :all
-    else 
+    else
       can :manage, User, id: user.id
+    end
 
     # Define abilities for the passed in user here. For example:
     #
