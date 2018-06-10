5a1b39b82c27e009ddb9603cae15dd97d18f6f30
Benjamin <cunningham1030@yahoo.com>
2018-06-10 16:32:36 +0200

    6.14 (5.3) Troubleshooting google mailer...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            '<cunningham103090>',
+    user_name:            '<cunningham103090@gmail.com>',
     password:             '<qncatbtbjrwhfghr>',
     authentication:       'plain',
     enable_starttls_auto: true }
